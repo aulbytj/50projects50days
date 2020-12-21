9aaf7f3e3153ccb11a421c0bf7759d37b3ec181b
Aulbourn <aulbourn@gmail.com>
2020-12-20 19:10:39 -0500

    add js for functionality

---
+++ src/scroll-animation/js/script.js
@@ -0,0 +1,23 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+  const triggerBottom = window.innerHeight / 5 * 3
+
+  boxes.forEach(box => {
+
+    // 'getBoundingClientRect' returns the size and position of an element relative to the viewport.
+
+    // the 'top' property will return the position of the top of the element
+    const boxTop = box.getBoundingClientRect().top
+
+    if (boxTop < triggerBottom) {
+      box.classList.add('show')
+    } else {
+      box.classList.remove('show')
+    }
+  })
+}

8dd6673a82b1907ade43fd65505188579dd7e3a8
Aulbourn <aulbourn@gmail.com>
2021-01-17 12:45:29 -0500

    add js, screenshotand styles

---
+++ src/sound-board/js/script.js
@@ -0,0 +1,23 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach(sound => {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+
+  btn.addEventListener('click', () => {
+    stopSound()
+    document.getElementById(sound).play()
+  })
+
+  btn.innerText = sound
+  document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSound() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound)
+
+    song.pause();
+    song.currentTime = 0;
+  })
+}

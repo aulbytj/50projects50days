59a9177001555e7c53f31e4538cc547ad23e8a4e
Aulbourn Knowles <aulbourn@gmail.com>
2021-04-22 16:40:49 -0400

    completed the faq boxes project

---
+++ src/faq-boxes/js/script.js
@@ -0,0 +1,9 @@
+const toggles = document.querySelectorAll('.faq-toggle')
+
+console.log(toggles)
+
+toggles.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    toggle.parentNode.classList.toggle('active')
+  })
+});

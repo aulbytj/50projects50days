9591dc4899ce2c0f16ce83d73d3adc8a7c5cae08
Aulbourn Knowles <aulbourn@gmail.com>
2021-05-17 13:34:06 -0400

    add random selection functionality

--- src/choice-picker-ui/js/script.js
+++ src/choice-picker-ui/js/script.js
@@ -7,7 +7,17 @@ textarea.focus()
 textarea.addEventListener('keyup', function(e) {
   // e.target.value will get the value of what is typed in the the textarea
   createTags(e.target.value)
+
+  if (e.key === 'Enter') {
+    setTimeout(function() {
+      e.target.value = ''
+    }, 10)
+    randomSelection()
+  }
 })
+
+
+
 
 function createTags(input) {
   const tags = input.split(',') // will split the values after a comma into array elements
@@ -24,3 +34,43 @@ function createTags(input) {
   });
 }
 
+function randomSelection() {
+  const times = 30
+
+  const interval = setInterval(function() {
+    const randomTag = pickRandomTag()
+
+    highlightTag(randomTag)
+
+    setTimeout(function() {
+      unhighlightTag(randomTag)
+    }, 100)
+  }, 100)
+
+  setTimeout(function() {
+    clearInterval(interval)
+
+    setTimeout(function() {
+      const randomTag = pickRandomTag()
+
+      highlightTag(randomTag)
+      
+    }, 100)
+  }, times * 100)
+  // clearInterval(interval)
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+
+function highlightTag(tag) {
+  tag.classList.add('highlight')
+}
+
+
+function unhighlightTag(tag) {
+  tag.classList.remove('highlight')
+}
